a68a88cef61fb7bcc78629d58a99f97ea562398f
HelenWTY <73513059+HelenWTY@users.noreply.github.com>
2021-11-14 00:33:42 -0800

    SRS & Ratio Estimation / Regression Estimation
    SRS & Ratio Estimation / Regression Estimation

--- srsRatioAndRegression.R
+++ srsRatioAndRegression.R
@@ -29,7 +29,7 @@ xbar.pop <- 5541 / 6 # cost on diet is $5541 per year (2 terms & 6 months)
 # ratio estimation
 ybar.srs <- mean(y.sample)
 xbar.srs <- mean(x.sample)
-ybar.ratio <- (ybar.srs / xbar.srs) * xbar.pop      # ratio estimation
+ybar.ratio <- (ybar.srs / xbar.srs) * xbar.pop
 e.ratio <- y.sample - (ybar.srs / xbar.srs) * x.sample
 s.e.sq <- 1 / (n-1) * sum(e.ratio^2)
 se.ratio <- sqrt((1 - n/N) * (s.e.sq / n))
@@ -38,7 +38,7 @@ CI.srs.ratio <- ybar.ratio + 1.96 * c(-se.ratio, se.ratio)
 
 # regression estimation
 lm.regression <- lm(y.sample~x.sample)
-summary(lm.regression)
+summary(lm.regression) # slope(b1.hat): 2.9457, intercept(b0.hat): 67.0118
 ybar.regression <- 2.9457 * xbar.pop + 67.0118
 e.regression <- resid(lm.regression)
 se.regression <- sqrt((1 - n/N)*(var(e.regression) / n))
