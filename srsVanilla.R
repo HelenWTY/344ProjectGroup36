cc68db14b63c8199e7cd11cb9e60fa67972a580e
HelenWTY <73513059+HelenWTY@users.noreply.github.com>
2021-11-13 23:08:05 -0800

    SRS & Vanilla Estimation
    SRS & Vanilla Estimation

---
+++ srsVanilla.R
@@ -0,0 +1,19 @@
+# SRS -vanilla estimation
+library("readxl")
+N <- 5996
+consumption <- read_excel("consumptionUBC.xlsx")
+
+# clean data
+consumption$`Expenses(month)` <- as.numeric(consumption$`Expenses(month)`)
+consumption <- na.omit(consumption)
+y.sample <- consumption$`Expenses(month)`
+n <- length(y.sample)
+
+# vanilla estimate
+ybar.srs <- mean(y.sample)
+se.srs <- sqrt((1 - n / N) * var(y.sample) / n)
+srs.vanilla <- c(ybar.srs, se.srs)
+CI.srs.vanilla <- ybar.srs + 1.96 * c(-se.srs, se.srs) 
+srs.vanilla
+CI.srs.vanilla
+
